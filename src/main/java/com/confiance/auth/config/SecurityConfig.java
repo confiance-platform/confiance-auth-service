badb30481f7eadf02a7f3e715e60ae17cee07bbd
rahulb15 <rahul.baghel1508@gmail.com>
2026-01-26 18:17:21 +0530

    version chnages

--- src/main/java/com/confiance/auth/config/SecurityConfig.java
+++ src/main/java/com/confiance/auth/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/**",
+                                "/api/v1/version/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
